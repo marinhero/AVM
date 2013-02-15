0bc8976023631ce75a8a29d8d30e0d11e7ea6690
Sergio Baez <sergio.baez231@gmail.com>
2013-02-15 11:03:14 +0100

    adding the private instances

---
+++ Int8.cpp
@@ -0,0 +1,13 @@
+//
+// Int8.cpp for abstractvm in /Users/sabs231/Documents/EPITECH/Classes/C++/Projects/AbstractVM/abstractvm
+//
+// Made by sergioandres baezserrano
+// Login   <baezse_s@epitech.net>
+//
+// Started on  Fri Feb 15 11:00:54 2013 sergioandres baezserrano
+// Last update Fri Feb 15 11:01:04 2013 sergioandres baezserrano
+//
+
+#include "Int8.hh"
+
+
---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// main.cpp for abstractvm in /Users/sabs231/Documents/EPITECH/Classes/C++/Projects/AbstractVM/abstractvm
+//
+// Made by sergioandres baezserrano
+// Login   <baezse_s@epitech.net>
+//
+// Started on  Fri Feb 15 10:13:33 2013 sergioandres baezserrano
+// Last update Fri Feb 15 10:14:00 2013 sergioandres baezserrano
+//
+
+#include <cstdint>
+
+int main()
+{
+  return (0);
+}
